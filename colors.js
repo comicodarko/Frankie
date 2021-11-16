c3f6e79cbc229950a1ffa29fea7a69bc139f78ec
comicodarko <bitotajima@hotmail.com>
2021-11-16 13:33:53 -0300

    🎨 - Cores definidas

---
+++ colors.js
@@ -0,0 +1,10 @@
+module.exports = {
+  black: '#282a36',
+  blue: '#8be9fd',
+  gray: '#bfbfbf',
+  green: '#50fa7b',
+  pink: '#ff80bf',
+  purple: '#bd93f9',
+  red: '#ff5555',
+  yellow: '#f1fa8c'
+}

4602975f0de7af598c2927ee47803290e136aaf3
comic <bitotajima@hotmail.com>
2022-02-16 10:48:06 -0300

    🚧 - Back-end / GetMovie - Agora retorna também onde assitir o filme

--- back-end/components/tmdb/getMovie.js
+++ back-end/components/tmdb/getMovie.js
@@ -11,13 +11,37 @@ module.exports = async (movie) => {
     }).then(res => {
         if(res.data.results.length > 0) {
             return axios.get(`https://api.themoviedb.org/3/movie/${res.data.results[0].id}`, {
-                params: { api_key: process.env.TMDB_API_KEY, language: 'pt-BR', }
+                params: { api_key: process.env.TMDB_API_KEY, language: 'pt-BR' }
             }).then(async res => {
-                const { title,  genres, original_title, runtime, release_date, 
+                const { id, title,  genres, original_title, runtime, release_date, 
                     imdb_id, budget, poster_path, backdrop_path, status, tagline, overview} = res.data;
-                    
-                return { title,  genres, original_title, runtime, release_date,
-                    imdb_id, budget, poster_path, backdrop_path, status, tagline, overview };
+                
+					axios.get(`https://api.themoviedb.org/3/movie/${id}/watch/providers`, {
+						params: { api_key: process.env.TMDB_API_KEY }
+					}).then(result => {
+						if(result.data.results.BR) {
+							if(result.data.results.BR.flatrate) {
+								let providers = [];
+								result.data.results.BR.flatrate.forEach((provider => {
+									const obj = {
+										...provider,
+										logo_path: `https://image.tmdb.org/t/p/w1280${provider.logo_path}`
+									}
+									provider.push(obj);
+								}))
+
+								return { title, genres, original_title, runtime, release_date,
+									imdb_id, budget, poster_path, backdrop_path, status, tagline, overview, providers }
+							} else {
+								return { title, genres, original_title, runtime, release_date,
+									imdb_id, budget, poster_path, backdrop_path, status, tagline, overview, providers: [] }
+							}
+						} else {
+							return { title, genres, original_title, runtime, release_date,
+								imdb_id, budget, poster_path, backdrop_path, status, tagline, overview, providers: [] }
+						}
+					})
+					return false
             })
         } else {
             return false
